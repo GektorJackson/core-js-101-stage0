cbf8b9385d6b92a3f025405286b135e81b96eaa7
macair <macair@GektorJackson.local>
2025-08-31 21:19:24 +0400

    feat: fix function

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -65,7 +65,7 @@ function getStringFromTemplate(firstName, lastName) {
  *   'Hello, John Doe!' => 'John Doe'
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
-function extractNameFromTemplate(value ) {
+function extractNameFromTemplate(value) {
   return value.slice(7, -1);
 }
 
